e39852f15ee8d4d2be18e43c52bf0171fd04f67b
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-10 14:41:06 +0530

    creating multiple nested element

--- app.js
+++ app.js
@@ -5,17 +5,22 @@
  *          <h1>This is h1 element</h1>
  *          <h2>This is h2 element</h2>
  *      </div>
+ *      <div id="child2">
+ *          <h1>This is h1 element</h1>
+ *          <h2>This is h2 element</h2>
+ *      </div>
  *  </div>
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is h1 element"),
     React.createElement("h2", {}, "This is h2 element"),
-  ])
-);
-
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is h1 element"),
+    React.createElement("h2", {}, "This is h2 element"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
