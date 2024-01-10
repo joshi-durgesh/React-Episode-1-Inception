2c3474cf6a9b28a90e05ebddc1d7440769e1a457
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-10 14:38:01 +0530

    nested create element

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World! form React."
+/**
+ * To crate nested elements like this
+ *  <div id="parent">
+ *      <div id="child">
+ *          <h1>This is h1 element</h1>
+ *          <h2>This is h2 element</h2>
+ *      </div>
+ *  </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is h1 element"),
+    React.createElement("h2", {}, "This is h2 element"),
+  ])
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
